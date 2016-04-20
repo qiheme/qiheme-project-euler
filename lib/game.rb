3697a4434d86e4a2f9e9dc6c05a11c5f486a1f3a
Quincy Iheme <qiheme@gmail.com>
2016-04-20 07:02:58 -0400

    Add tests for straight

--- lib/game.rb
+++ lib/game.rb
@@ -10,13 +10,41 @@ class Game
 
   def is_flush?(hand)
     flush = false
+
     while !flush
       %w(S C H D).each do |suit|
         i = 0
         hand.each {|card| i += 1 if card["#{suit}"] == suit  }
         flush = true if i == 5
       end
-      return flush
     end
+
+    return flush
+  end
+
+  def is_straight?(hand)
+    straight = false
+    ranks = %w(A K Q J 10 9 8 7 6 5 4 3 2)
+
+    card_positions = hand.map do |card|
+      card_split = card.split('')
+      if card_split.length == 3
+        card_split[0] += card_split[1]
+        card_split.delete_at(1)
+      end
+      ranks.index(card_split.first)
+    end
+
+    card_positions = card_positions.sort
+    i = card_positions.first
+
+    card_positions.each do |index|
+      while i == index
+        i += 1
+      end
+    end
+
+    straight = true if i - 1 == card_positions[-1]
+    return straight
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -14,18 +14,26 @@ describe "Game" do
 
   describe "different hands" do
     context "royal flush" do
-      let(:flush_spades) { ["QS", "10S", "JS", "KS", "AS"] }
-      let(:flush_hearts) { ["QH", "10H", "JH", "KH", "AH"] }
-      let(:flush_clubs) { ["QC", "10C", "JC", "KC", "AC"] }
-      let(:flush_diamonds) { ["QD", "10D", "JD", "KD", "AD"] }
-      let(:not_flush) { ["QS", "10D", "JC", "KH", "AS"] }
+      let(:royal_flush_spades) { ["QS", "10S", "JS", "KS", "AS"] }
+      let(:royal_flush_hearts) { ["QH", "10H", "JH", "KH", "AH"] }
+      let(:royal_flush_clubs) { ["QC", "10C", "JC", "KC", "AC"] }
+      let(:royal_flush_diamonds) { ["QD", "10D", "JD", "KD", "AD"] }
+      let(:not_royal_flush) { ["10S", "9D", "JC", "6H", "AS"] }
 
       it "has all cards of the same suit" do
-        expect(game.is_flush?(flush_spades)).to be true
-        expect(game.is_flush?(flush_hearts)).to be true
-        expect(game.is_flush?(flush_clubs)).to be true
-        expect(game.is_flush?(flush_diamonds)).to be true
-        expect(game.is_flush?(not_flush)).to be false
+        expect(game.is_flush?(royal_flush_spades)).to be true
+        expect(game.is_flush?(royal_flush_hearts)).to be true
+        expect(game.is_flush?(royal_flush_clubs)).to be true
+        expect(game.is_flush?(royal_flush_diamonds)).to be true
+        expect(game.is_flush?(not_royal_flush)).to be false
+      end
+
+      it "has all cards sequentially in order" do
+        expect(game.is_straight?(royal_flush_spades)).to be true
+        expect(game.is_straight?(royal_flush_hearts)).to be true
+        expect(game.is_straight?(royal_flush_clubs)).to be true
+        expect(game.is_straight?(royal_flush_diamonds)).to be true
+        expect(game.is_straight?(not_royal_flush)).to be false
       end
     end
   end
